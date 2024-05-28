d19c088c60afafc12ae9a5f50bf163a1ec7f0a47
6c0 <lykioo@protonmail.com>
2024-05-28 17:02:57 +0200

    Initial commit

---
+++ src/client/java/fr/sixczero/HudMiniClient.java
@@ -0,0 +1,12 @@
+package fr.sixczero;
+
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.rendering.v1.HudRenderCallback;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.font.TextRenderer;
+import net.minecraft.client.gui.DrawContext;
+
+public class HudMiniClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() { }
+}
