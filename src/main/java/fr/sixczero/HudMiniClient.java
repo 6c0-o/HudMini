b22729fd41c5da4bc3eee2e1d381e206d8240c3f
6c0-o <lykioo@protonmail.com>
2024-09-03 14:26:46 +0200

    Restructure

--- src/main/java/fr/sixczero/HudMiniClient.java
+++ src/main/java/fr/sixczero/HudMiniClient.java
@@ -5,8 +5,8 @@ import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.rendering.v1.HudRenderCallback;
 
 public class HudMiniClient implements ClientModInitializer {
-	@Override
-	public void onInitializeClient() {
-		HudRenderCallback.EVENT.register(new hudLoader());
-	}
+    @Override
+    public void onInitializeClient() {
+        HudRenderCallback.EVENT.register(new hudLoader());
+    }
 }
